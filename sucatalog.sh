16a999cc7b724b54b87d40b06dd12a70e1bffa6d
Per Olofsson <magervalp@fastmail.fm>
2015-08-06 13:28:48 +0200

    gunzip catalogs.

--- sucatalog.sh
+++ sucatalog.sh
@@ -3,4 +3,7 @@
 while read -r url; do
     echo "$url"
     curl -O "$url"
+    if [[ -f *.sucatalog.gz ]]; then
+        gunzip -f *.sucatalog.gz
+    fi
 done < <(strings /System/Library/PrivateFrameworks/SoftwareUpdate.framework/SoftwareUpdate | grep '^http' | grep sucatalog)
