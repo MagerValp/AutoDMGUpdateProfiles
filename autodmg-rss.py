05f5010d065ff0d566add829084ba46a063ba38f
Nate Felton <n8felton@gmail.com>
2017-01-12 06:11:11 -0500

    Updated RSS helper tool to generate AutoDMG profiles. (#52)
    Uses code from autopkg/n8felton-recipes AppleSupportDownloadInfoProvider.py to help find download links.

--- autodmg-rss.py
+++ autodmg-rss.py
@@ -1,82 +1,98 @@
-#!/usr/bin/python
-#
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+"""Helper tool to generate AutoDMG profiles."""
 
-
+import xml.etree.ElementTree as ET
 import sys
-import argparse
-import urllib2
-from xml.etree import ElementTree
 import re
+import urllib2
+import plistlib
 
+FEED_URL = u'http://rss.support.apple.com/?channel=DOWNLOADS'
 
-FEED_URL = u"http://rss.support.apple.com/?channel=DOWNLOADS"
+INCLUDED_UPDATES = ['Update',
+                    'Safari',
+                    'iTunes',]
 
+EXCLUDED_UPDATES = ['Windows',
+                    'Firmware',
+                    'Combo',]
 
-def printf8(f, *args):
-    print >>f, " ".join(unicode(x).encode(u"utf-8") for x in args)
-
-
-re_url = re.compile(ur"download_link.setAttribute\('href',\s*'(?P<url>.+\.dmg)'\)")
-
-def main(argv):
-    p = argparse.ArgumentParser()
-    p.add_argument(u"-v", u"--verbose", action=u"store_true",
-                   help=u"Verbose output.")
-    p.add_argument(u"updates", help=u"plist with updates")
-    args = p.parse_args(argv[1:])
-    
-    if args.verbose:
-        print >>sys.stderr, u"Downloading %s" % FEED_URL
+def get_download_url(link):
+    """Follows HTTP 302 redirects to fetch the final url of a download."""
+    base_url = "https://support.apple.com"
+    article_number = link.split('/')[-1]
+    download_url = ("{base_url}/downloads/{article_number}/en_US/".format(
+        base_url=base_url,
+        article_number=article_number))
     try:
-        f = urllib2.urlopen(FEED_URL)
-        feed = ElementTree.parse(f)
+        request = urllib2.Request(download_url)
+        response = urllib2.urlopen(request)
+    except BaseException as e:
+        sys.exit("Can't download %s: %s" % (download_url, e))
+
+    filename = response.geturl().split('/')[-1]
+    return "{}{}".format(download_url, filename)
+
+def get_itunes_download_url(title):
+    """Takes the title of an iTunes update and attempts to find a suitble
+    download link from the iTunes download page.
+    """
+    re_version = re.compile(ur"(?P<version>(?:(\d+)\.)?(?:(\d+)\.)?(\d+)(.*))")
+    itunes_base_url = "https://swdlp.apple.com/iframes/82/en_us/82_en_us.html"
+
+    # Figure out which version we need a url for
+    version_match = re_version.search(title)
+    if version_match:
+        version = version_match.group('version')
+
+    # Build a regex to include the version number
+    re_url = re.compile(ur"value='(https://.*?iTunes{}.dmg)'".format(version))
+
+    try:
+        html = urllib2.urlopen(itunes_base_url).read()
     except urllib2.URLError as e:
         sys.exit(u"Feed fetching failed: %s" % unicode(e))
-    except ElementTree.ParseError as e:
+
+    url_match = re_url.search(html)
+    if url_match:
+        return url_match.group(1)
+
+def main():
+    """Where all the magic happens"""
+    try:
+        rss_feed = urllib2.urlopen(FEED_URL)
+        rss_feed = ET.parse(rss_feed)
+    except urllib2.URLError as e:
+        sys.exit(u"Feed fetching failed: %s" % unicode(e))
+    except ET.ParseError as e:
         sys.exit(u"Feed parsing failed: %s" % unicode(e))
-    
-    with open(args.updates, "w") as f:
-        printf8(f, u'<?xml version="1.0" encoding="UTF-8"?>')
-        printf8(f, u'<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">')
-        printf8(f, u'<plist version="1.0">')
-        printf8(f, u'<dict>')
-        printf8(f, u'\t<key>Updates</key>')
-        printf8(f, u'\t<dict>')
-        root = feed.getroot()
-        for item in root.findall(u"channel/item"):
-            name = item.find(u"title").text
-            if name.startswith(u"iOS") or name.startswith(u"iTunes") or u"Windows" in name:
-                continue
-            kb_url = item.find(u"link").text
-            if args.verbose:
-                print u"Downloading %s" % kb_url
-            try:
-                html = urllib2.urlopen(kb_url).read()
-            except urllib2.URLError as e:
-                sys.exit(u"Feed fetching failed: %s" % unicode(e))
-            m = re_url.search(html)
-            if m:
-                url = m.group(u"url")
-                if url.startswith(u"/downloads"):
-                    url = u"http://support.apple.com" + url
-                printf8(f, u"\t\t<key></key>")
-                printf8(f, u"\t\t<dict>")
-                printf8(f, u"\t\t\t<key>name</key>")
-                printf8(f, u"\t\t\t<string>%s</string>" % name)
-                printf8(f, u"\t\t\t<key>url</key>")
-                printf8(f, u"\t\t\t<string>%s</string>" % url)
-                printf8(f, u"\t\t</dict>")
-            else:
-                print >>sys.stderr, u"No download button found for %s" % name
-        
-        printf8(f, u'\t</dict>')
-        printf8(f, u'</dict>')
-        printf8(f, u'</plist>')
-    
-    return 0
-    
+
+    updates_found = {"Updates": {}}
+
+    for item in rss_feed.findall(".//item"):
+        title = item.find('title').text
+        # Only includes updates with 'INCLUDED_UPDATES' in the title.
+        # Filters out 'EXCLUDED_UPDATES' for things we don't care about.
+        if (not any(update in title for update in INCLUDED_UPDATES) or
+                any(update in title for update in EXCLUDED_UPDATES)):
+            continue
+
+        link = item.find(u'link').text
+        url = get_download_url(link)
+
+        if (".dmg" or ".pkg") not in url:
+            if "iTunes" in title:
+                url = get_itunes_download_url(title)
+
+        print "{}\n{}".format(title, url)
+        update = {"name": title,
+                  "url": url,
+                 }
+
+        updates_found["Updates"][link.split('/')[-1]] = update
+
+    plistlib.writePlist(updates_found, "updates.plist")
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
-    
+    main()
