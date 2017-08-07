31bc98b98aaba546c730d5dfe6b6fb5825b144a8
Per Olofsson <magervalp@fastmail.fm>
2017-08-07 22:01:59 +0200

    Silence sucatalog warnings

--- sucatalog.sh
+++ sucatalog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -f *.sucatalog *.sucatalog.gz
 while read -r url; do
     echo "$url"
     curl -O "$url"
-    gunzip -f *.sucatalog.gz
+    gunzip -f *.sucatalog.gz 2>/dev/null
 done < <(strings /System/Library/PrivateFrameworks/SoftwareUpdate.framework/SoftwareUpdate | grep '^http' | grep sucatalog)
