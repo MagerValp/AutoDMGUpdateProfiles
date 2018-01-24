c013513195eb93c376981c2fb9f8c1c718fc1d9c
Per Olofsson <magervalp@fastmail.fm>
2018-01-24 11:10:34 +0100

    10.13.3 17D47.

--- autodmg-verify.py
+++ autodmg-verify.py
@@ -28,6 +28,44 @@ def uprint(*args, **kwargs):
     print(*encoded_objects, **kwargs)
 
 
+re_build = re.compile(ur'^(?P<major>\d+)(?P<minor>[A-Z])(?P<build>.+)$')
+
+def verify_verbuild(verbuild):
+    ver, _, build = verbuild.partition(u"-")
+    vers = list(int(x) for x in ver.split(u"."))
+    if len(vers) not in [2, 3]:
+        uprint(u"ERROR: Bad version: %s" % verbuild)
+        return False
+    if vers[0] != 10:
+        uprint(u"ERROR: Bad version: %s" % verbuild)
+        return False
+    major = vers[1]
+    try:
+        minor = vers[2]
+    except IndexError:
+        minor = 0
+    if (major < 7) or (major > 16):
+        uprint(u"ERROR: Bad version: %s" % verbuild)
+        return False
+    if (minor < 0) or (minor > 15):
+        uprint(u"ERROR: Bad version: %s" % verbuild)
+        return False
+    m = re_build.search(build)
+    if not m:
+        uprint(u"ERROR: Bad build: %s" % verbuild)
+        return False
+    build_major = int(m.group(u"major"))
+    build_minor = ord(m.group(u"minor")) - 0x41
+    build_build = int(m.group(u"build"))
+    if build_major != major + 4:
+        uprint(u"ERROR: build major does not match OS major: %s" % verbuild)
+        return False
+    if build_minor != minor:
+        uprint(u"ERROR: build minor does not match OS minor: %s" % verbuild)
+        return False
+    return True
+
+
 def main(argv):
     p = argparse.ArgumentParser()
     p.add_argument(u"-v", u"--verbose", action=u"store_true",
@@ -57,6 +95,8 @@ def main(argv):
     supported_builds = set(x.partition(u"-")[2] for x in plist[u"DeprecatedInstallers"].iterkeys())
     re_osbuild = re.compile(ur'^\d+[A-Z]\d+[a-z]?$')
     for replacement, replaced in plist[u"DeprecatedInstallers"].iteritems():
+        if not verify_verbuild(replacement):
+            error_count += 1
         for build in replaced:
             if not re_osbuild.search(build):
                 uprint(u"Error: invalid deprecated build %s" % build)
@@ -74,6 +114,8 @@ def main(argv):
     known_updates = set(plist[u"Updates"].iterkeys())
     referenced_updates = set()
     for profile, updates in plist[u"Profiles"].iteritems():
+        if not verify_verbuild(profile):
+            error_count += 1
         for update in updates:
             if not update in known_updates:
                 uprint(u"Error: Profile %s references unknown update: %s" % (profile, update))
